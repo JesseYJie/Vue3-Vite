7b47d313b7c341f5a48896e01a30dfae52b25f70
docker <59284346+JesseYJie@users.noreply.github.com>
2024-03-05 17:12:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { resolve } from "path"
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -10,59 +10,59 @@ import Inspect from 'vite-plugin-inspect'
 import pxtovw from 'postcss-px-to-viewport'
 
 const loder_pxtovw = pxtovw({
-  //这里是设计稿宽度 自己修改
-  viewportWidth: 2100,
+  // 这里是设计稿宽度 自己修改
+  viewportWidth: 1920,
   viewportUnit: 'vw',
-  selectorBlackList: [""]
 })
 
-export default defineConfig(() => {
-  return {
-    css: {
-      postcss: {
-        plugins: [loder_pxtovw]
-      }
+// https://vitejs.dev/config/
+export default defineConfig(() => ({
+  css: {
+    postcss: {
+      plugins: [loder_pxtovw],
     },
-    server: {
-      port: 8080,
-      host: '0.0.0.0'
-    },
-    resolve: {
-      alias: [{
+  },
+  server: {
+    port: 8080,
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: [
+      {
         find: '@',
-        replacement: resolve(__dirname, "src"),
-      }]
-    },
-    build: {
-      minify: "terser",
-      terserOptions: {
-        compress: {
-          drop_console: true,
-        }
-      }
-    },
-    plugins: [
-      vue(),
-      AutoImport({
-        resolvers: [
-          ElementPlusResolver(),
-          IconsResolver({
-            prefix: 'Icon',
-          })
-        ],
-      }),
-      Components({
-        resolvers: [
-          ElementPlusResolver(),
-          IconsResolver({
-            enabledCollections: ['ep'],
-          })
-        ],
-      }),
-      Icons({
-        autoInstall: true,
-      }),
-      Inspect()
+        replacement: resolve(__dirname, 'src'),
+      },
     ],
-  }
-})
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+      ],
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+    Inspect(),
+  ],
+}))
